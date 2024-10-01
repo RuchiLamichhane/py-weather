c01e432e75ba55209fb5e32214f96cae248a94a9
RuchiLamichhane <ruchilamichhane2468@gmail.com>
2024-10-01 15:36:12 +0545

    changes

--- weather.py
+++ weather.py
@@ -6,6 +6,8 @@ url= f'https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={API
 response = requests.get(url)
 if response.status_code ==200:
      data= response.json()
-     print('weather is',data['weather'][0]['description'])
+     print('Weather is',data['weather'][0]['description'])
      print('Current Temperature is',data['main']['temp'])
      print('Current Humidity is',data['main']['humidity'])
+else:
+     print('No City Found')
